445c60658afcb82a8c973e70132d4f1c2891c046
Fisher <32707008+FisherWY@users.noreply.github.com>
2019-10-11 11:11:33 +0800

    进程的撤销完成
    1. 将createProcess.cpp更名为process.cpp
    2. 进程的撤销完成：首先将进程从进程树上剥离，再递归撤销进程及其子进程，同时将进程从进程总链上剥离

--- process.cpp
+++ process.cpp
@@ -14,7 +14,7 @@ pnode *plink;
 int createpc(int *para)
 {
 	/**
-	 * p: 进程链表plink的操作指针
+	 * p: 操作指针
 	 * p1: 新进程的指针
 	 * pp: 新进程p1的父节点
 	 **/
@@ -65,6 +65,114 @@ int createpc(int *para)
 	for (p=plink; p->next; p=p->next);
 	p->next = p1;
 
+	return 0;
+}
+
+// 从节点p开始删除进程树
+void deltree(pnode *tbd)
+{
+	for (pnode *p = plink; p; p = p->next)
+	{
+		if (p->next!=NULL && p->next==tbd)
+		{
+			p->next = tbd->next;
+			break;
+		}
+	}
+	if (tbd->brother != NULL)
+	{
+		deltree(tbd->brother);
+	}
+	if (tbd->sub != NULL)
+	{
+		deltree(tbd->sub);
+	}
+	delete tbd;
+}
+
+/**
+ * 撤销进程
+ * para[0]: 要撤销的进程pid
+ **/
+int deletepc(int *para)
+{
+	if (para[0] == 0)
+	{
+		printf("Root process(pid:0) can not be deleted!\n");
+		return -1;
+	}
+
+	/**
+	 * p: 工作指针
+	 * plink_previous: 要撤销的进程在进程链表中的前一个节点
+	 * ptree_previous: 要撤销的进程在进程树中的前一个节点
+	 * tbd: 要撤销的进程节点
+	 * pflag: 记录要撤销的进程是否查找成功
+	 * tbd_ppid: 要撤销的进程的父进程
+	 **/
+	pnode *p, *ptree_previous, *tbd;
+	int pflag = 0, tbd_ppid = -1;
+
+	// 找到要撤销的进程
+	for (p=plink; p; p=p->next)
+	{
+		if (p->node->pid == para[0])
+		{
+			tbd_ppid = p->node->ppid;
+			tbd = p;
+			pflag = 1;
+			break;
+		}
+	}
+
+	// 找到要撤销的进程的父进程
+	for (p=plink; p; p=p->next)
+	{
+		if (p->node->pid == tbd_ppid)
+		{
+			ptree_previous = p;
+			break;
+		}
+	}
+
+	if (!pflag)
+	{
+		printf("Process pid %d not found!\n", para[0]);
+		return -2;
+	}
+
+	if (tbd_ppid < 0)
+	{
+		printf("Process parent(pid:%d) not found!\n", tbd_ppid);
+		return -3;
+	}
+
+	// 将进程从进程树中删除
+	if (ptree_previous != NULL)
+	{
+		// 进程是父进程的第一个字节点
+		if (ptree_previous->sub == tbd)
+		{
+			ptree_previous->sub = tbd->brother;
+			tbd->brother=NULL;
+		}
+		// 进程是子节点但不是第一个
+		else
+		{
+			for (p=ptree_previous->sub; p; p=p->brother)
+			{
+				if (p->brother == tbd)
+				{
+					p->brother = tbd->brother;
+					tbd->brother = NULL;
+					break;
+				}
+			}
+		}
+	}
+
+	// 将进程及其子进程从进程链表中删除，然后从内存中删除
+	deltree(tbd);
 	return 0;
 }
 
@@ -132,6 +240,9 @@ int main(int argc, char const *argv[])
 			cflag = 1;
 			pflag = 1;
 			printf("创建新进程: createpc(pid,ppid,prio)\n");
+			printf("撤销进程: deletepc(pid)\n");
+			printf("获取帮助: help\n");
+			printf("退出: exit\n");
 		}
 
 		// 创建新进程，判断createpc是否为cmdstr字串
@@ -150,6 +261,26 @@ int main(int argc, char const *argv[])
 			para = (int *) malloc(3);
 			para = strtoarray(s);
 			createpc(para);
+
+			pflag = 1;
+		}
+
+		// 撤销进程
+		if (strstr(cmdstr, "deletepc"))
+		{
+			int *para;
+			char *s;
+
+			cflag = 1;
+
+			int start = instr(cmdstr, '(');
+			int end = instr(cmdstr, ')');
+			s = substr(cmdstr, start+1, end-1);
+
+			para = (int *) malloc(1);
+			para = strtoarray(s);
+
+			deletepc(para);
 
 			pflag = 1;
 		}
